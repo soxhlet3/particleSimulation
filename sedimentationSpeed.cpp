067c214a0562bec4f913433376b733d96cf60b2b
H. Minh Pham <hm.pham@outlook.de>
2021-06-27 01:00:18 +0200

    float --> double

--- sedimentationSpeed.cpp
+++ sedimentationSpeed.cpp
@@ -52,9 +52,9 @@ double get_sedimentationSpeed(double pDiameter, double pDensity, double fDensity
 	double Re = 0;
 	double cw = 1;
 	double currentSpeed = 0;
-   sedimentationSpeed = sqrt(4*(pDensity-fDensity)*g*pDiameter/(3*fDensity))*sqrt(1/cw);
-   
-	do {
+	
+	sedimentationSpeed = sqrt(4*(pDensity-fDensity)*g*pDiameter/(3*fDensity))*sqrt(1/cw);
+   do {
 		Re = sedimentationSpeed*pDiameter/fKinVis;
 		cout << "Current Re = " << Re << endl;
 	
